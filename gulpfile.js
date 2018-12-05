cc1decac0028d2b81543f7489c9736079861ff20
James Dinh <jamesdinh14@gmail.com>
2018-12-05 15:22:37 -0500

    Let Protractor spin up standalone Selenium server

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 var protractor = require("gulp-protractor").protractor;
-var webdriver_standalone = require("gulp-protractor").webdriver_standalone;
 var webdriver_update = require("gulp-protractor").webdriver_update_specific;
 var runSequence = require("run-sequence");
 
@@ -15,25 +14,22 @@ gulp.task("build", function() {
 
 // Download selenium webdriver
 gulp.task("webdriver_update", webdriver_update({
-    browsers: ["ignore_ssl"]
+    webdriverManagerArgs: ["--ignore_ssl"]
 }));
 
-// Start standalone selenium server
-gulp.task("webdriver_standalone", webdriver_standalone);
-
-gulp.task("timesheet", ["webdriver_update"], function() {
+gulp.task("timesheet", function() {
     return gulp.src([])
         .pipe(protractor({
             configFile: "./dist/src/conf.js",
             args: []
         })).on("error", function (err) {
             throw err;
-        }).on("end", function() {
-
         });
 });
 
-gulp.task("default", ["build"]);
+gulp.task("default", function() {
+    runSequence("build", "timesheet");
+});
 
 gulp.task("watch", ["build"], function() {
     gulp.watch('src/*.ts', function() {
