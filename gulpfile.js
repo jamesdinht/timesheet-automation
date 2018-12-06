d0cfebc2dddfb4a8e9dfbb51094351dc130e130d
MenjiShad <jamesdinh14@gmail.com>
2018-12-05 22:53:34 -0500

    Add linting to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var tsProject = ts.createProject("tsconfig.json");
 var protractor = require("gulp-protractor").protractor;
 var webdriver_update = require("gulp-protractor").webdriver_update_specific;
 var runSequence = require("run-sequence");
+var gulpTslint = require("gulp-tslint");
+var tslint = require("tslint");
 
 gulp.task("build", function() {
     return tsProject.src()
@@ -17,7 +19,7 @@ gulp.task("webdriver_update", webdriver_update({
     webdriverManagerArgs: ["--ignore_ssl"]
 }));
 
-gulp.task("timesheet", function() {
+gulp.task("timesheet", ["webdriver_update"], function() {
     return gulp.src([])
         .pipe(protractor({
             configFile: "./dist/src/conf.js",
@@ -27,12 +29,19 @@ gulp.task("timesheet", function() {
         });
 });
 
+gulp.task("lint:ts", function() {
+    var program = tslint.Linter.createProgram("./tsconfig.json")
+    return gulp.src("src/*.ts")
+        .pipe(gulpTslint({ program }))
+        .pipe(gulpTslint.report())
+});
+
 gulp.task("default", function() {
     runSequence("build", "timesheet");
 });
 
 gulp.task("watch", ["build"], function() {
     gulp.watch('src/*.ts', function() {
-        runSequence("build", "timesheet");
+        runSequence("build", "lint:ts", "timesheet");
     });
 });
