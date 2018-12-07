b0ee24eb070a3c90d0fbafb99efd2c0d7d6e95e9
Steven <stevenalfred93@yahoo.com>
2018-12-06 23:53:03 -0500

    Added automating email sending. Formatting still needs work. Does not allign properly

--- ComposeEmail.py
+++ ComposeEmail.py
@@ -1,4 +1,5 @@
 import datetime
+import smtplib
 import UserEmailInfo
 
 daysOfTheWeek = [] 
@@ -21,6 +22,8 @@ def constructDaysOfTheWeek():
 
 def email_template():
     hours = 0
+    email = ('\nHere are my hours worked for the week of ' + str(firstDay.month) + "/" + str(firstDay.day) + "-" 
+    + str(lastDay.month) + '/' + str(lastDay.day) +":\n")
     print ('\nHere are my hours worked for the week of ' + str(firstDay.month) + "/" + str(firstDay.day) + "-" 
     + str(lastDay.month) + '/' + str(lastDay.day) +":")
     for x in reversed(daysOfTheWeek):
@@ -30,16 +33,45 @@ def email_template():
         year = str(x.year)
         hours +=8 # Will eventually add based on user input for hours worked per day
         print("")
+        email += "\n"
         output = '{:15}{:5}{:3}{:10}{:3}{}'.format(dayName,months,day,year,str(UserEmailInfo.defaultHours),"hours")
         print(output)
+        email +=output 
+        email += "\n"
     print ('\nTotal Hours: ' + str(hours) + ' hours')
+    email += ('\nTotal Hours: ' + str(hours) + ' hours\n')
     print (UserEmailInfo.signature)
+    email += (UserEmailInfo.signature)
+    return email
    
 
 constructDaysOfTheWeek()
 firstDay = daysOfTheWeek[4]
 lastDay = daysOfTheWeek[0]
 
-email_template()
 
+
+mail = email_template()
+print("\nHere is the line split\n")
+print (mail)
+
+test_email = """From: %s
+To: %s
+Subject: %s
+
+%s
+""" %(UserEmailInfo.user_email,', '.join(UserEmailInfo.to),UserEmailInfo.subject,mail)
+
+try:
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo() #identify to the SMTP server
+    server.starttls()#begin secure connection
+    server.login(UserEmailInfo.user_email,UserEmailInfo.user_pass)
+    server.sendmail(UserEmailInfo.user_email,UserEmailInfo.to,test_email)
+    server.close()
+
+    print('Email Successfully Sent?')
+except:
+    print("Something went wrong")
+    
 
